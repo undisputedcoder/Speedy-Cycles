d5c3e405f97fb2361fb31cb6d5bc15a7809754c3
Riaz <riaz_amin@iinet.net.au>
2022-01-04 23:18:51 +0800

    Changed layout of website

--- js/render/bikes.js
+++ js/render/bikes.js
@@ -7,7 +7,7 @@ function renderBikes() {
             <img class="product-image" src="${product.img}">
             <div id="product-info" class="product-info">
                 <p id="name" class="product-name">${product.name}</p>
-                <p id="price" class="product-price">${product.price}</p>
+                <p id="price" class="product-price">$${product.price}</p>
             </div>    
             <div>
                 <button id="add" class="add">Add to cart</button>
